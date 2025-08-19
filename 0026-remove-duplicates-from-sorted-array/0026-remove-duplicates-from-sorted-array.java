0597243d0ed6dc7e13ed9455ee1a4306069bd176
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-19 22:37:04 +0530

    Time: 0 ms (100%), Space: 44.7 MB (91.51%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -14,13 +14,24 @@ class Solution {
         // }
         // return i+1;
 
-        int i=0;
-        for(int j=1;j<nums.length;j++){
-            if(nums[i]!=nums[j]){
-                i++;
-                nums[i]=nums[j];
-            }
+        // int i=0;
+        // for(int j=1;j<nums.length;j++){
+        //     if(nums[i]!=nums[j]){
+        //         i++;
+        //         nums[i]=nums[j];
+        //     }
+        // }
+        // return i+1;
+
+    if (nums.length == 0) return 0;
+    int k = 1; // Start from second element
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] != nums[k - 1]) {
+            nums[k] = nums[i];
+            k++;
         }
-        return i+1;
+    }
+    return k;
+
     }
 }
