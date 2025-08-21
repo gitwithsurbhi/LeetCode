de0116b23c90ab94756257b7540bae0b17dbb099
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-21 20:14:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,9 +3,8 @@ class Solution {
        int left=0;
        for(int right=0;right<nums.length;right++){
         if(nums[right]!=0){
-            int temp=nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
+          nums[left]=nums[right];
+            nums[right]=0;
             left++;
         }
        }
