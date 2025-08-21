99ef44c61dcd33dab347e4042f16d4f6a133abb4
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-21 20:16:53 +0530

    Time: 180 ms (5.01%), Space: 46.4 MB (24.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,12 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int left=0;
-       for(int right=0;right<nums.length;right++){
-        if(nums[right]!=0){
-          nums[left]=nums[right];
-            nums[right]=0;
-            left++;
+    //    int left=0;
+    //    for(int right=0;right<nums.length;right++){
+    //     if(nums[right]!=0){
+    //         int temp=nums[left];
+    //         nums[left]=nums[right];
+    //         nums[right]=temp;
+    //         left++;
+    //     }
+    //    }
+
+    int insertPos=0;
+    for(int num:nums){
+        if(num!=0){
+            nums[insertPos++]=num;
         }
-       }
+    }
+    while(insertPos<nums.length){
+        nums[insertPos++]=0;
+    }
     }
 }
