f8ff22a27990729182c0f812cec3903b57d8c279
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-23 23:22:32 +0530

    Time: 1 ms (27.04%), Space: 41.8 MB (40.05%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -0,0 +1,34 @@
+class Solution {
+    private boolean isAlphabet(char ch){
+        if((ch >=65 && ch<=90) || (ch>=97 && ch<=122) ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public String reverseOnlyLetters(String s) {
+        StringBuilder sb=new StringBuilder(s);
+      int i=0,j=s.length()-1;
+      while(i<=j){
+      
+        if((isAlphabet(sb.charAt(i)) ) && (isAlphabet(sb.charAt(j)) )){
+            char temp=sb.charAt(i);
+            sb.setCharAt(i,s.charAt(j));
+            sb.setCharAt(j,temp);
+            i++;
+            j--;
+       
+       }
+       else  if(!isAlphabet(sb.charAt(i))){
+            i++;
+        }
+       else if(!isAlphabet(sb.charAt(j))){
+            j--;
+        }
+       
+      }
+     return sb.toString();  
+    }
+    
+}
