a7b5645ddbfb8ba25f4742989838482a83eae2a7
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-25 15:59:39 +0530

    Time: 1 ms (99.84%), Space: 46.1 MB (72.93%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        // Corner/edge case
+        if(nums.length==1)return nums[0];
+        int xor=0;
+        for(int num:nums){
+           xor^=num;
+        }
+   return xor;
+    }
+}
