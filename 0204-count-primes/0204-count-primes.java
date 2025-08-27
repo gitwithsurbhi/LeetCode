8d3337eead14daebc3dbe9477e55438717080bf4
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-27 22:09:05 +0530

    Time: 356 ms (5.03%), Space: 49.2 MB (70.52%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean []isPrime=new boolean[n+1];
+       Arrays.fill(isPrime,true);
+       int ans=0; 
+       for(int i=2;i<n;i++){
+        if(isPrime[i]){
+            ans++;
+        }
+        for(int j=i*2;j<n;j+=i){
+            isPrime[j]=false;
+        }
+       }
+       return ans;
+    }
+}
