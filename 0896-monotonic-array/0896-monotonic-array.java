2c173b7f25b0b1413657c0d890dc8161d3e85c10
Surbhi Sarathe <surbhisarathe384@gmail.com>
2025-08-30 22:47:53 +0530

    Time: 4 ms (6.03%), Space: 56.7 MB (98.74%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -3,6 +3,8 @@ class Solution {
      boolean inc=true,dec=true;
      for(int i=1;i<nums.length;i++){
         if(nums[i]>nums[i-1])inc=false;
+     }
+      for(int i=1;i<nums.length;i++){
         if(nums[i-1]>nums[i])dec=false;
      }
      return inc|| dec;
