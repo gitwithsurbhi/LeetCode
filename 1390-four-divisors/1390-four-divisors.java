48520881859265142551f2e886b443e85c495ce4
Surbhi Sarathe <surbhisarathe384@gmail.com>
2026-01-05 10:11:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+   //Brute force approach
+   int currSum=0;
+   for(int i=0;i<nums.length;i++){
+     int count=0;
+     int sum=0;
+     for(int j=1;j*j<=nums[i];j++){
+        if(nums[i]%j==0){
+            count++;
+            sum+=j;
+        }
+        if(count>4){
+            break;
+        }
+     }
+     if(count == 4){
+         currSum+=sum;
+     }
+     } 
+
+     
+     return currSum;
+    }
+}
