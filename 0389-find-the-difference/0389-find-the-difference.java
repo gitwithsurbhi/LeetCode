bac31a136d6b484210379eb9da7107146fbce862
Surbhi Sarathe <surbhisarathe384@gmail.com>
2026-01-29 14:55:24 +0530

    Time: 3 ms (39.74%), Space: 43.2 MB (46.47%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,29 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        //brute force
+        int []freq=new int[26];
+       
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(freq[ch-'a']>0){
+               freq[ch-'a']--;
+            }
+            else{
+                freq[ch-'a']++;
+            }
+           
+        }
+        char ans='\0';
+        for(int i=0;i<26;i++){
+            if(freq[i]==1){
+                 ans= (char)('a'+ i);
+               
+            }
+        }
+        return ans;
+    }
+}
