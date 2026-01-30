a1d70478f490f65de8a150ac51a42c1e0c590fff
Surbhi Sarathe <surbhisarathe384@gmail.com>
2026-01-30 20:13:58 +0530

    Time: 34 ms (25.91%), Space: 48.7 MB (26.81%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+     int n=candyType.length/2;
+     HashSet<Integer>set=new HashSet<>();
+     for(int i=0;i<candyType.length;i++){
+        set.add(candyType[i]);
+     } 
+     if(set.size()>=n){
+        return n;
+     } 
+     else{
+          return set.size();
+     } 
+
+    }
+}
