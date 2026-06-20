b60c9392117807d0ae8ce5f9aab20f49111d19d7
Surbhi Sarathe <surbhisarathe384@gmail.com>
2026-06-20 13:32:45 +0530

    Time: 0 ms (100%), Space: 44.2 MB (93.05%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxProduct(int[] nums) {
+        /*
       PriorityQueue<Integer>pq=new PriorityQueue<>(Comparator.reverseOrder());
         for(int i=0;i<nums.length;i++){
             pq.add(nums[i]);
@@ -9,5 +10,18 @@ class Solution {
     pq.remove();
     prod*=pq.peek()-1;
     return prod;
+    */
+    int max1=0;
+    int max2=0;
+    for(int num:nums){
+        if(num>max1){
+            max2=max1;
+            max1=num;
+         }
+         else if(num > max2){
+            max2=num;
+         }
+    }
+    return (max1-1)*(max2-1);
     }
 }
